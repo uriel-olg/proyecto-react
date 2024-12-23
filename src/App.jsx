aa78a0a5030ffd101705bcfdfb4ea7f153370b50
uriel277 <113132534+uriel277@users.noreply.github.com>
2024-12-23 18:50:40 -0300

    se creo la lista de cripto monedas,se agrego axios y el archivo css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,37 @@
-import './App.css'
+import { useEffect ,useState } from "react"
+import axios from "axios"
+
 
 function App() {
-  <h1>Hola mundo</h1>
+
+  //const API_URL = import.meta.env.VITE_API_URL
+  const url = "https://api.coinlore.net/api/tickers/"
+  const [criptos,] = useState([])
+
+  useEffect(()=>{
+    axios.get(url)
+    .then((data)=>{
+      console.log(data)
+      //setCriptos(data.data)
+    })
+    .catch(()=>{
+      console.error("algo ah fallado")
+    })
+  },[])
+
+  if(criptos.length ===0) return <span>Cargando...</span>
+
+  return(
+    <>
+      <h1>Lista de criptos</h1>
+      <ol>
+        {
+        criptos.map(({id, name,price_usd }) =>(
+          <li key={id} > nombre:{name}, Precio$:{price_usd}</li>
+        ))
+      }
+      </ol>
+    </>
+  )  
 }
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <>
+  <App />
   
-    <App />
   
+  </>
+    
 )
